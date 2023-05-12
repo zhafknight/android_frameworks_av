3f7869e73d631254fc5fdf5164571bcdca3165f1
Dominggoes Isakh <drjisakh@gmail.com>
2023-05-12 22:56:56 +0200

    Revert "Camera: Fix missing physical camera availability callback"
    This reverts commit 3d316f37cc755289bdfd8a4c70d9e36a40e7e64d.
    
    Signed-off-by: Dominggoes Isakh <drjisakh@gmail.com>

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -203,7 +203,6 @@ status_t CameraService::enumerateProviders() {
     status_t res;
 
     std::vector<std::string> deviceIds;
-    std::unordered_map<std::string, std::set<std::string>> unavailPhysicalIds;
     {
         Mutex::Autolock l(mServiceLock);
 
@@ -234,7 +233,7 @@ status_t CameraService::enumerateProviders() {
             ALOGE("Failed to enumerate flash units: %s (%d)", strerror(-res), res);
         }
 
-        deviceIds = mCameraProviderManager->getCameraDeviceIds(&unavailPhysicalIds);
+        deviceIds = mCameraProviderManager->getCameraDeviceIds();
     }
 
 
@@ -242,12 +241,6 @@ status_t CameraService::enumerateProviders() {
         String8 id8 = String8(cameraId.c_str());
         if (getCameraState(id8) == nullptr) {
             onDeviceStatusChanged(id8, CameraDeviceStatus::PRESENT);
-        }
-        if (unavailPhysicalIds.count(cameraId) > 0) {
-            for (const auto& physicalId : unavailPhysicalIds[cameraId]) {
-                String8 physicalId8 = String8(physicalId.c_str());
-                onDeviceStatusChanged(id8, physicalId8, CameraDeviceStatus::NOT_PRESENT);
-            }
         }
     }
 
@@ -504,7 +497,7 @@ void CameraService::onDeviceStatusChanged(const String8& id,
 
     if (state == nullptr) {
         ALOGE("%s: Physical camera id %s status change on a non-present ID %s",
-                __FUNCTION__, physicalId.string(), id.string());
+                __FUNCTION__, id.string(), physicalId.string());
         return;
     }
 
