591d6687086a97594ce4841872e62b95e7fd1ab4
Dominggoes Isakh <drjisakh@gmail.com>
2023-08-05 17:27:29 +0200

    Revert "DO NOT MERGE Camera: Enable session parameter handling for Rotate&Crop"
    This reverts commit b9f8381c7c4e7dc07ba7cf778f6cd24b55b28005.
    
    Signed-off-by: Dominggoes Isakh <drjisakh@gmail.com>

--- services/camera/libcameraservice/device3/Camera3Device.h
+++ services/camera/libcameraservice/device3/Camera3Device.h
@@ -581,9 +581,6 @@ class Camera3Device :
         // overriding of ROTATE_AND_CROP value and adjustment of coordinates
         // in several other controls in both the request and the result
         bool                                mRotateAndCropAuto;
-        // Indicates that the ROTATE_AND_CROP value within 'mSettingsList' was modified
-        // irrespective of the original value.
-        bool                                mRotateAndCropChanged = false;
 
         // Whether this capture request has its zoom ratio set to 1.0x before
         // the framework overrides it for camera HAL consumption.
@@ -770,11 +767,6 @@ class Camera3Device :
      */
     static nsecs_t getMonoToBoottimeOffset();
 
-    // Override rotate_and_crop control if needed
-    static bool    overrideAutoRotateAndCrop(const sp<CaptureRequest> &request /*out*/,
-            bool overrideToPortrait,
-            camera_metadata_enum_android_scaler_rotate_and_crop_t rotateAndCropOverride);
-
     struct RequestTrigger {
         // Metadata tag number, e.g. android.control.aePrecaptureTrigger
         uint32_t metadataTag;
@@ -925,7 +917,7 @@ class Camera3Device :
         status_t           addFakeTriggerIds(const sp<CaptureRequest> &request);
 
         // Override rotate_and_crop control if needed; returns true if the current value was changed
-        bool               overrideAutoRotateAndCrop(const sp<CaptureRequest> &request /*out*/);
+        bool               overrideAutoRotateAndCrop(const sp<CaptureRequest> &request);
 
         // Override test_pattern control if needed for camera mute; returns true
         // if the current value was changed
@@ -1364,8 +1356,6 @@ class Camera3Device :
     // Whether the camera framework overrides the device characteristics for
     // app compatibility reasons.
     bool mOverrideToPortrait;
-    camera_metadata_enum_android_scaler_rotate_and_crop_t mRotateAndCropOverride;
-    bool mComposerOutput;
 
     // Current active physical id of the logical multi-camera, if any
     std::string mActivePhysicalId;
